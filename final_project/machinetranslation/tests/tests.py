5bdb5763a4aa008c1213d665555f4b5657aa6ad6
db314 <>
2023-06-11 19:53:10 +0200

    translations implemented

---
+++ final_project/machinetranslation/tests/tests.py
@@ -0,0 +1,19 @@
+import unittest
+
+from translator import english_to_french, french_to_english
+
+
+class TestF2E(unittest.TestCase):
+    def testCase1(self):
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+    def testCase2(self):
+        self.assertEqual(french_to_english('bière'), 'beer')
+
+class TestE2F(unittest.TestCase):
+    def testCase(self):
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+    def testCase2(self):
+        self.assertEqual(english_to_french('beer'), 'bière')
+
+if __name__ == '__main__':
+    unittest.main()
